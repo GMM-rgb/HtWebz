fea12101880698b519565eec32725b3c6df20e25
Maximus Farvour <maximus.farvour@outlook.com>
2025-03-24 13:37:38 -0700

    Improved Navigation System

--- index/assets/scripts/navigation_managment.js
+++ index/assets/scripts/navigation_managment.js
@@ -1,9 +1,15 @@
 let searcherButton = document.querySelector("#searcherAppImageButton");
 let searcherApp = document.querySelector("#searcherApp");
 
+let openingApp = false;
+
 searcherButton.onclick = () => {
-    notify("Opening Searcher App...");
-    wait(1000).then(() => {
-        window.open('index/search_index.html', '_blank');
-    });
+    if(openingApp === true) {return; } else {
+        notify("Opening Searcher App...");
+        openingApp = true;
+        wait(1000).then(() => {
+            window.open('index/search_index.html', '_blank');
+            openingApp = false;
+        });
+    }
 }
