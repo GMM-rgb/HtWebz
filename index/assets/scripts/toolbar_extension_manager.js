655ae07667033a68c22ecd8f898f820e831074f6
Maximus Farvour <maximus.farvour@outlook.com>
2025-04-01 15:41:07 -0700

    Arranged Index.html And Commented out Code section in toolbar Extension manager

--- index/assets/scripts/toolbar_extension_manager.js
+++ index/assets/scripts/toolbar_extension_manager.js
@@ -1,17 +1,17 @@
-let appButtonContainerWarp = document.getElementById('appButtonContainerWarp');
+// let appButtonContainerWarp = document.getElementById('appButtonContainerWarp');
 
-var buttonArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var buttonArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-window.onload = () => {
-    // Remove the unnecessary inner loop and use the correct forEach method
-    buttonArray.forEach(btnValue => {
-        console.log(btnValue);
-        const appContainerButton = document.createElement('button');
-        appContainerButton.setAttribute('class', 'wbt-toolbar-button');
-        appContainerButton.innerHTML = '...' + btnValue;  
-        appButtonContainerWarp.appendChild(appContainerButton);  
-    });
-}
+// window.onload = () => {
+//     // Remove the unnecessary inner loop and use the correct forEach method
+//     buttonArray.forEach(btnValue => {
+//         console.log(btnValue);
+//         const appContainerButton = document.createElement('button');
+//         appContainerButton.setAttribute('class', 'wbt-toolbar-button');
+//         appContainerButton.innerHTML = '...' + btnValue;  
+//         appButtonContainerWarp.appendChild(appContainerButton);  
+//     });
+// }
 
 let animationClass = "expandMenuAnimation"; // Animation name for the toolbarMenuExtension
 let animationDuration = 500; // Duration in milliseconds
