bde89a15aaae0682af9a71463df33d0d5d0a113c
Maximus Farvour <maximus.farvour@outlook.com>
2025-04-04 12:10:47 -0700

    Updated page minimap preview system script

--- index/assets/scripts/page_minimap_preview_system.js
+++ index/assets/scripts/page_minimap_preview_system.js
@@ -1,3 +1,9 @@
 let previewMiniMapCanvas = document.getElementById("mapPreviewPageCanvas");
 
+let ctx = previewMiniMapCanvas.getContext("2d");
+
+if (!ctx) {
+    return console.error("Failed to get 2D context for the minimap preview canvas.");
+}
+
 
