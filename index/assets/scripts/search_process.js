177e699fc97b21ff56ea9440c567957709d17755
GMM-rgb <maximus.farvour@outlook.com>
2025-09-23 11:34:50 -0700

    Updated Search Process to import variables and info from search_interface.js

--- index/assets/scripts/search_process.js
+++ index/assets/scripts/search_process.js
@@ -1,6 +1,8 @@
 ﻿let MaxRetryAttempts = 3;
 let RetryAttempts = 0;
 
+let SubmittedSearchQuery = null;
+
 window.addEventListener("DOMContentLoaded", (e) => {
     const SearchInput = document.getElementById("SearchBarInput");
     while (!SearchInput) {
@@ -10,11 +12,18 @@ window.addEventListener("DOMContentLoaded", (e) => {
     }
     let SearchValueText = SearchInput.value || null;
 
-    SearchInput.addEventListener("keydown", (e) => {
+    SearchInput.addEventListener("keypress", (e) => {
+        e.stopPropagation();
         if (SearchInput.textContent && SearchInput.value) {
             SearchValueText = SearchInput.value;
         }
+    });
+
+    SearchInput.addEventListener("keydown", (e) => {
         e.stopPropagation();
+        if (e.key === "Enter" || e.key === "Return") {
+            SubmittedSearchQuery = SearchValueText;
+        }
     });
 
     e.stopPropagation();
