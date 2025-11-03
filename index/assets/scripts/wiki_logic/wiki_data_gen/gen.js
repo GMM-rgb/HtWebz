e3ef0fd32c65d547829c77296e93096bdb0f5d56
Maximus Farvour <6041464@luhsd.net>
2025-11-03 15:14:40 -0800

    Updated Wiki Gen and Parse Data scripts.

--- index/assets/scripts/wiki_logic/wiki_data_gen/gen.js
+++ index/assets/scripts/wiki_logic/wiki_data_gen/gen.js
@@ -1,10 +1,30 @@
-import { JSON_wiki_data } from "./modules/parse_json_data";
+import { JSON_wiki_data, LoadJSON } from "./modules/parse_json_data";
 const contentFrame = document.getElementById("wiki-content-frame");
+
+let Retryed = false;
 
 function generateWikiTopicOverview() {
     if (!contentFrame || !(contentFrame instanceof HTMLDivElement || HTMLElement)) return;
 
-    
+    if (JSON_wiki_data) {
+        try {
+
+        } catch (GenerationError) {
+            console.error("ERROR: Wiki Generation Output Failed.\t", GenerationError);
+            if (!Retryed && (typeof LoadJSON === "function")) {
+                JSON_wiki_data = LoadJSON?.();
+                generateWikiTopicOverview();
+                Retryed = true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
 
-if (JSON_wiki_data) window.addEventListener("DOMContentLoaded", generateWikiTopicOverview);
+window.addEventListener("DOMContentLoaded", () => {
+    let Generation_Sucess = generateWikiTopicOverview?.() ? JSON_wiki_data : console.warn("JSON Wiki Data was null.");
+    if (Generation_Sucess) {
+        console.log("Gen Success.");
+    }
+});
--- index/assets/scripts/wiki_logic/wiki_data_gen/modules/parse_json_data.js
+++ index/assets/scripts/wiki_logic/wiki_data_gen/modules/parse_json_data.js
@@ -1,3 +1,15 @@
-export let JSON_wiki_data = null;
+/**
+ * @type {Object?}
+ */
+let JSON_wiki_data = null;
 
+/**
+ * @returns {JSON | Object ?}
+ */
+function LoadJSON() {
 
+}
+
+JSON_wiki_data = LoadJSON?.() ?? console.error("LoadJSON function unknown.");
+
+export { JSON_wiki_data, LoadJSON };
