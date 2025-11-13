6fb37540dfa2c843e6639f28fe00b1d6de21adf4
Maximus Farvour <6041464@luhsd.net>
2025-11-13 10:21:40 -0800

    Updated datastore backend script.

--- backend/datastore_backend_system.js
+++ backend/datastore_backend_system.js
@@ -1,9 +1,11 @@
+const picocolors = require('picocolors');
 const path = require('path');
 const fs = require('fs');
 
 const dataDirectory = path.join(__dirname, 'data');
 const usersFilePath = path.join(dataDirectory, 'users.json');
 
+// Create the data folder directory if non-existent
 if (!fs.existsSync(dataDirectory)) {
     fs.mkdirSync(dataDirectory);
 }
@@ -30,5 +32,24 @@ module.exports = {
         const users = this.getUsers();
         users.users[user.id] = user;
         fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+    },
+    removeUser: function(userToRemove) {
+        if (!userToRemove || userToRemove === null) return false;
+        let SuccessfullyRemovedUser = false;
+
+        if (this.getUsers instanceof Function) {
+            const UserData = this.getUsers();
+
+
+        } else {
+            console.warn(picocolors.yellow(`WARNING: function: getUsers is not a function, or either invalid or missing.`));
+        }
+
+        if (SuccessfullyRemovedUser) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
+
