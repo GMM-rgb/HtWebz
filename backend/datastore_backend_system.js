4778d7a1c835303259cc9b08671d4ff5b8d3ba10
Maximus Farvour <maximus.farvour@outlook.com>
2025-11-13 08:06:01 -0800

    Updated datastore backend system with new jsdocs

--- backend/datastore_backend_system.js
+++ backend/datastore_backend_system.js
@@ -10,17 +10,25 @@ if (!fs.existsSync(dataDirectory)) {
 
 // Initialize users.json if it doesn't exist
 if (!fs.existsSync(usersFilePath)) {
-    fs.writeFileSync(usersFilePath, JSON.stringify([]));
+    fs.writeFileSync(usersFilePath, JSON.stringify({ users: {}}, null, 2));
 }
 
 module.exports = {
+    /**
+     * 
+     * @returns {Object} The users object
+     */
     getUsers: function() {
         const usersData = fs.readFileSync(usersFilePath);
         return JSON.parse(usersData);
     },
+    /**
+     * 
+     * @param {Object} user - The user object to add
+     */
     addUser: function(user) {
         const users = this.getUsers();
-        users.push(user);
+        users.users[user.id] = user;
         fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
     }
 };
