f6982edd339c95d13261e23cf839e9e18254892a
Maximus Farvour <6041464@luhsd.net>
2025-12-09 13:34:25 -0800

    Added base foundation for video player interaction.

--- public/index/assets/scripts/video_explorer_logic/video_player_interaction.js
+++ public/index/assets/scripts/video_explorer_logic/video_player_interaction.js
@@ -1,3 +1,20 @@
-const VideoFeedPlayback = document.getElementById("VideoFeedPlaybackView");
+/**
+ * @type {HTMLDivElement?} */
+const VideoPlayer = document.getElementById("VideoPlayer");
+/**
+ * @type {HTMLVideoElement?} */
+const VideoFeedPlayback = VideoPlayer.querySelector("#VideoFeedPlaybackView");
+/**
+ * @type {HTMLSpanElement?} */
+const VideoControls = VideoPlayer.querySelector("#VideoPlayerControls:nth-child(1)");
 
+window.addEventListener("DOMContentLoaded", () => {
+    /**
+     * @type {HTMLButtonElement?} */
+    let PlayButton = VideoControls.querySelector(".video-playback-toggle");
 
+    VideoFeedPlayback.addEventListener("timeupdate", (e) => {
+        e.stopPropagation();
+        const CurrentTimestamp = e.timeStamp || 0;
+    });
+}, { once: true });
