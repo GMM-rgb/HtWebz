a2eb8c31217bbbe4b03700c3c5a1ca4541d58da8
Maximus Farvour <maximus.farvour@outlook.com>
2025-12-30 18:14:07 -0800

    Added new Development Utilitys in client scripting utils; such as a ReportClientError function.

--- public/index/assets/scripts/modules/JavaScript_Utility_Extras/scripting_utils.js
+++ public/index/assets/scripts/modules/JavaScript_Utility_Extras/scripting_utils.js
@@ -1,20 +1,34 @@
+const SocketConnection = io();
 class SchedulerUtilitys {
     /**
      * 
-     * 
+     * @param {...any} args
      * @returns {void}
      */
-    static WaitForCondition() {
-        let AllowContinue = false;
-        try {
+    static WaitForCondition(...args) {
+        let CanContinue = false;
+        (async () => {
+            try {
 
-        } catch (ConditionWaitError) {
-            console.error(`The function %cWaitForCondition%c, encountered an error: ${ConditionWaitError}`, "color: yellow;", "color: normal;");
-            return false;
-        } finally {
-            return (AllowContinue !== null && AllowContinue === true) ? AllowContinue : false;
-        }
+            } catch (ConditionWaitError) {
+                console.error(`The function %cWaitForCondition%c, encountered an error: ${ConditionWaitError}`, "color: yellow;", "color: normal;");
+                return false;
+            } finally {
+                return (CanContinue !== null && CanContinue === true) ? CanContinue : false;
+            }
+        });
     }
 }
 
-export { SchedulerUtilitys };
+class DevelopmentUtilitys {
+    /**
+     * 
+     * @param {string} ERROR_Message 
+     */
+    static ReportClientError(ERROR_Message) {
+        if (!ERROR_Message || !(typeof ERROR_Message === "string")) return;
+        SocketConnection.emit("ClientErrorReport", `${ERROR_Message}`);
+    }
+}
+
+export { SchedulerUtilitys, DevelopmentUtilitys };
