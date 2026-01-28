0824ddd2b22e7d9dfd2169f1a73ecf64e9ba65f4
Maximus Farvour <6041464@luhsd.net>
2026-01-27 16:41:14 -0800

    Updated SearchEngine Backend Logistics

--- backend/server_search_system/client_search_handler.js
+++ backend/server_search_system/client_search_handler.js
@@ -1,5 +1,33 @@
 // NodeJS Modules
 const fs = require("fs");
 const path = require("path");
-//
 
+class SearchProcesser {
+    /**
+     * 
+     */
+    static RequestSearchEngineSegmentData() {
+        
+    }
+    /**
+     * @param {string} RequestedSearchString
+     * 
+     * @returns {JSON?}
+     */
+    static async ProcessSearch(RequestedSearchString) {
+        console.log(`${this.name.toString()}: `);
+        //
+        let PulledSearchData = {};
+        //
+        if (RequestedSearchString !== null && (RequestedSearchString instanceof String)) {
+            const SearchArgumentWordAmount = RequestedSearchString.split(" ").length;
+        }
+        // Returns back the search data; that was pulled from SearchEngine JSON.
+        if (PulledSearchData === null) return null;
+        return PulledSearchData;
+    }
+}
+
+module.exports = {
+
+};
