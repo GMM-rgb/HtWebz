5a0118541c2a780a45e0dbabe7050a9189b1b6df
Maximus Farvour <6041464@luhsd.net>
2026-01-29 12:55:27 -0800

    Updated Search Processer on DatabaseFetcher class.

--- backend/server_search_system/search_system_modules/search_processer.js
+++ backend/server_search_system/search_system_modules/search_processer.js
@@ -8,7 +8,7 @@ class DatabaseFetcher {
      * Fetches the `SearchEngineDatabase`; to be indexed in search process.
      * @returns {Promise<{
      *   DatabaseDirectory: string,
-     *   FetchedFileData: JSON
+     *   FetchedFileData: JSON,
      * }?>}
      */
     static FetchDatabase = async function() {
@@ -16,11 +16,71 @@ class DatabaseFetcher {
         //
         if (fs !== null && path !== null) {
             /**
+             * Fetches a Database file from the requested input parameter.
+             * @param {string} RequestedFileName
+             */
+            async function FetchDatabaseFile(RequestedFileName) {
+
+            }
+            /**
+             * Fetches the directory of the SearchEngineDatabase & returns the name of it.
+             * @returns {Promise<string?>}
+             */
+            async function GetDatabaseDirectory() {
+                const DatabasePossibleFileNames = {
+                    DatabaseMapperJSON: "search_engine_database_mapper",
+                };
+
+                /**
+                 * @type {string?}
+                 */
+                var Directory = null;
+                /**
+                 * @type {string[]?}
+                 */
+                var SearchSystemDirectorysAndFiles = fs.readdirSync(path.join("../"));
+                /**
+                 * @returns {string?}
+                 */
+                function FindDatabaseDirectory() {
+                    var FD = null;
+
+                    if (SearchSystemDirectorysAndFiles !== null && (SearchSystemDirectorysAndFiles instanceof Array)) {
+                        for (let ReadDirIndex = 0; ReadDirIndex < SearchSystemDirectorysAndFiles.length; ReadDirIndex++) {
+                            
+                        }
+                    }
+
+                    return FD;
+                }
+
+                // Ensures that the directory contents can be accessed.
+                try {
+                    var FsConstants = fs.constants;
+                    fs.accessSync("search_engine_database", FsConstants.R_OK | FsConstants.W_OK);
+                    
+                } catch (PermissionError) {
+                    console.error(`Could not access SearchEngineDatabase directory, permmisions inssuficent!\nERROR:\n${PermissionError}`);
+                    return null;
+                }
+
+                if (Directory !== null && !(Directory instanceof String)) {
+                    return null;
+                } else {
+                    return Directory;
+                }
+            }
+            /**
              * @type {string?}
              */
-            var DatabaseDirectory = null;
-            
-
+            var DatabaseDirectory = await GetDatabaseDirectory();
+            /**
+             * @type {JSON?}
+             */
+            var FetchedData = null;
+            //
+            fs.readFileSync(null, "utf-8");
+            //
             return;
         } else {
             if (fs === null) console.warn(`[${FunctionName}]:\t`);
