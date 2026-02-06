2acde33960a9091d611cd150f32920d6986d23b4
GMM-rgb <maximus.farvour@outlook.com>
2026-02-06 14:00:25 -0800

    Added Conditional Check.

--- public/index/assets/scripts/input_format_tool_system/input_format_interface.js
+++ public/index/assets/scripts/input_format_tool_system/input_format_interface.js
@@ -1,5 +1,7 @@
 import * as InputScannerUtility from "./input_scanner";
-
+// 
 InputScannerUtility.InputFormaterData.InputScanElementData.ScannedInputElements.forEach((InputElement) => {
-
+    if (InputElement !== null && InputElement instanceof HTMLInputElement) {
+        
+    }
 });
