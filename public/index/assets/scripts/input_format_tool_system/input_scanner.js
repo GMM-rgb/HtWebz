db58afcbe6dd22deb06a0a33425efe0e30a46a71
Maximus Farvour <maximus.farvour@outlook.com>
2026-02-06 10:51:55 -0800

    Refactored Input Scanner Utility to Apply EventListeners.

--- public/index/assets/scripts/input_format_tool_system/input_scanner.js
+++ public/index/assets/scripts/input_format_tool_system/input_scanner.js
@@ -1,4 +1,5 @@
-const body = document.body;
+const body = document !== null ? document.body : null;
+const FetchedInputs = body.querySelectorAll("input");
 /**
  * 
  */
@@ -7,20 +8,75 @@ var InputElementData = {
     ScannedInputElements: []
 };
 /**
+ * 
+ * @param {boolean} FormatToolsEnabledState
+ */
+async function ToggleFormatTools(FormatToolsEnabledState) {
+    /**
+     * Triggers the display of the `HTMLInputElement` formating tools.  
+     * The boolean input type results:
+     * - `true` -> Enabled : Focusing Input
+     * - `false` -> Disabled : Not Focusing
+     * @returns {HTMLElement?}
+     */
+    function FormatToolsDisplay() {
+        try {
+            
+        } catch (FormatToolsStateError) {
+            if (FormatToolsEnabledState === true)
+                console.error(`Formating Tools encountered an error while setting up`);
+            if (FormatToolsEnabledState !== true)
+                console.error();
+            return void null;
+        }
+    }
+    // 
+    if (FormatToolsEnabledState !== null) {
+        if (typeof(FormatToolsEnabledState) === "boolean") {
+            if (FormatToolsEnabledState === true) {
+                console.debug(`Enabling Formating Tools...`);
+
+            } else {
+                console.debug(`Disabling Formating Tools...`);
+            }
+        }
+    }
+}
+/**
  * #### Scans for `<input/>` elements within the HTML document; the function is executed in.
  * @returns {void}
  */
 function ScanForInputsInDocument() {
     if (body !== null && (body instanceof HTMLElement)) {
-        for (var DocumentChildrenElementIndex = 0; DocumentChildrenElementIndex < body.childNodes.length; DocumentChildrenElementIndex++) {
-            const ScannedElement = body.childNodes.item(DocumentChildrenElementIndex);
-            if (ScannedElement !== null && (ScannedElement instanceof HTMLElement) && (ScannedElement instanceof HTMLInputElement)) {
-                if (InputElementData && InputElementData.InputElementAmount !== null) InputElementData.InputElementAmount += 1;
-                console.log(`Found new Input element within the page document.\nTotal Input Elements:\t${InputElementData.InputElementAmount.toString()}`);
-                
+        //for (var DocumentChildrenElementIndex = 0; DocumentChildrenElementIndex < body.childNodes.length; DocumentChildrenElementIndex++) {
+            //const ScannedElement = body.childNodes.item(DocumentChildrenElementIndex);
+            //if (ScannedElement !== null && (ScannedElement instanceof HTMLElement) && (ScannedElement instanceof HTMLInputElement)) {
+                //if (InputElementData && InputElementData.InputElementAmount !== null) InputElementData.InputElementAmount += 1;
+                //console.log(`Found new Input element within the page document.\nTotal Input Elements:\t${InputElementData.InputElementAmount.toString()}`);
+            //}
+        //}
+        /**
+         * 
+         * @param {HTMLInputElement} TargetInputElement
+         * @returns {EventListener[]?}
+         */
+        function ApplyInputListeners(TargetInputElement) {
+            let AppliedListeners = [];
+            if (TargetInputElement !== null && TargetInputElement instanceof HTMLInputElement) {
+                AppliedListeners.push(TargetInputElement.addEventListener("focus", (ev) => {
+
+                }));
             }
         }
+        // 
+        FetchedInputs.forEach((FetchedInput) => {
+            if (FetchedInput !== null && FetchedInput instanceof HTMLInputElement) {
+                if (ApplyInputListeners && typeof(ApplyInputListeners) === "function") {
+                    ApplyInputListeners();
+                }
+            }
+        });
     }
 }
-
+//
 self.addEventListener("change", ScanForInputsInDocument);
