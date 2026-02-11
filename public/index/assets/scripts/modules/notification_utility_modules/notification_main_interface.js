180068b9109078bce250fab17d9ae3c371a72174
GMM-rgb <maximus.farvour@outlook.com>
2026-02-11 14:30:49 -0800

    Refactored Notification Interface Main Script, for creating a new notification; when logic is fully completed in the future.

--- public/index/assets/scripts/modules/notification_utility_modules/notification_main_interface.js
+++ public/index/assets/scripts/modules/notification_utility_modules/notification_main_interface.js
@@ -1,15 +1,54 @@
+/**
+ * 
+ */
+class NotificationElementHolder {
+
+}
+
+/**
+ * 
+ */
+class NotificationInstanceBuilder {
+    static newNotificationObject() {
+
+    }
+}
+
+/**
+ * 
+ */
 class ActionNotification {
     /**
      * 
-     * @param {string} NotificationMessage 
+     * @param {string} NotificationMessage
+     * @param {boolean} AutoRemove 
      */
-    constructor(NotificationMessage) {
+    constructor(NotificationMessage, AutoRemove) {
+        /**
+         * 
+         * @type {String}
+         */
         this.message = new String(NotificationMessage).toString();
+        this.removingAutomatically = AutoRemove && new Boolean(AutoRemove).valueOf() || false;
     }
 
-    
+    async notify() {
+        try {
+
+        } catch (NotificationFailure) {
+            if (NotificationFailure !== null) {
+                const NotificationFailureMessage = new String(NotificationFailure).trimStart().valueOf();
+                throw new Error(`${NotificationFailureMessage}`);
+            }
+        }
+        if (this.message !== null && typeof(this.message) === "string") {
+
+        } else if (this.message === null || typeof(this.message) !== "string") {
+            console.warn(`Notification message was NULL, or invalid.\nExpected type literal:\t${String.name.toString()}`);
+        }
+    }
 }
 
 export {
-    ActionNotification,
+    ActionNotification
 };
