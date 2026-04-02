d7ee4f1b56d942aaacd95ad82146c1d3b77ae2d7
Maximus Farvour <6041464@luhsd.net>
2026-04-02 11:51:41 -0700

    Fixed stylesheet for the client search interface, due to class name changes.

--- public/index/assets/scripts/toolbar_interface_handler/navigation_toolbar_effects.js
+++ public/index/assets/scripts/toolbar_interface_handler/navigation_toolbar_effects.js
@@ -1,3 +1,2 @@
-const NavigationButtons = null;
-
-
+const NavigationButtons = document.getElementById("topUserInterfaceBar").querySelectorAll("div .navigation-interface-button");
+console.debug(String(NavigationButtons.length.valueOf()));
