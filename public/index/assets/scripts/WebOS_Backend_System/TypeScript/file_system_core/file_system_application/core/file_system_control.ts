d28e6e20062b9ce643d4697efdcee45920af3143
Maximus Farvour <6041464@luhsd.net>
2026-04-08 10:19:11 -0700

    Modified file system controler for the WebOS virtual machine.

--- public/index/assets/scripts/WebOS_Backend_System/TypeScript/file_system_core/file_system_application/core/file_system_control.ts
+++ public/index/assets/scripts/WebOS_Backend_System/TypeScript/file_system_core/file_system_application/core/file_system_control.ts
@@ -1,23 +1,32 @@
 /// <reference path="../../file_system_data_types.d.ts" />
+type ValidDataTypes = ["FILE", "DIRECTORY"];
+type FileTypes = [FileSystemDirectory, FileSystemStandardFile];
 class FileSystemWebOS implements FileSystemReference {
     /**
      * ---
-     * []
+     * ...
      */
-    constructor() {
+    constructor(private RequestedLocalFileSystemID: String) {
 
     }
 
-    static FetchData(): any
-    static FetchData(): any[]
-
-    static FetchData(): void {
-
+    static FetchData(ReferenceType: ValidDataTypes[1]): FileSystemDirectory;
+    static FetchData(ReferenceType: ValidDataTypes[0]): FileSystemStandardFile;
+    /**
+     * 
+     * @returns 
+     */
+    static FetchData(ReferenceType: (ValidDataTypes[0] | ValidDataTypes[1])): FileTypes[0] | FileTypes[1] | undefined {
+        let FetchedFileSystemData = null as FileTypes[0] | FileTypes[1] | null;
+        
+        return FetchedFileSystemData ?? undefined;
     }
 
-    public StartFileSystem(FileSystemID?: string): void {
+    public StartFileSystem(FileSystemID: string = this.RequestedLocalFileSystemID.valueOf()): void {
         if (FileSystemID !== undefined && typeof(FileSystemID) === "string") {
-
+            
         }
     }
 }
+
+// FileSystemWebOS.FetchData("DIRECTORY");
--- public/index/assets/scripts/WebOS_Backend_System/TypeScript/file_system_core/file_system_data_types.d.ts
+++ public/index/assets/scripts/WebOS_Backend_System/TypeScript/file_system_core/file_system_data_types.d.ts
@@ -1,5 +1,10 @@
 declare type FileSystemDirectory = {
+    name: String;
+    DirectoryParent?: FileSystemDirectory;
+};
 
+declare type FileSystemStandardFile = {
+    namme: String;
 };
 
 declare abstract class FileSystemReference {
