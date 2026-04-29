c2418adf7c7548520f2072f45d384d700eb34e99
Maximus Farvour <6041464@luhsd.net>
2026-04-29 10:44:23 -0700

    Revbised window docker construction instancer logistics.

--- public/index/assets/scripts/HtWebz_Window_Docker/Window_Docker_TypeScript/window_dock_constructor.ts
+++ public/index/assets/scripts/HtWebz_Window_Docker/Window_Docker_TypeScript/window_dock_constructor.ts
@@ -1,22 +1,33 @@
 /// <reference path="window_dock_objects.d.ts" />
-
 namespace HtWebzDockWindowStatistics {
     export let ActiveDockWindows: Array<typeof HtWebzDockWindow.prototype> = [];
     export let MinimizedDockWindows: Array<typeof HtWebzDockWindow.prototype> = [];
 }
 
 class HtWebzDockWindow implements WindowDockPrimative {
-    constructor(public DockWindowName: string) {
+    public WindowMenuMinimized: boolean;
+    private WindowDockShadowElement: HTMLUnknownElement | HTMLElement;
 
+    constructor(public WindowDockName: string, private StartMinimized: boolean = false) {
+        this.WindowMenuMinimized = new Boolean(StartMinimized ?? "false").valueOf();
+        this.WindowDockShadowElement = document.createElement("htwebz-dock-window", {
+            is: HTMLUnknownElement.name,
+        });
     }
 
     public setMinimized(NewMinimizedStatus: boolean): void {
         if (NewMinimizedStatus === undefined || !(typeof (NewMinimizedStatus) === "boolean")) return;
+        
     }
 
-    public removeWindowDock(): Promise<void> {
-        return new Promise((): void => {
+    public async removeWindowDock(): Promise<void> {
+        return new Promise<(void)>((): void => {
+            if (this.WindowDockShadowElement !== undefined && Object.is(this.WindowDockShadowElement.nodeName, "htwebz-dock-window")) {
 
-        });
+            }
+        }).catch((DockClosingError: Error) => {
+            if (DockClosingError == null || !(DockClosingError instanceof Error).valueOf()) return void undefined;
+            console.error(String("Crticial ERROR in closing WindowDock:\n" + "WindowDockName:\t" + new String(this.WindowDockName ?? undefined).trim() + "\n" + "Error Message:\t" + (DockClosingError.message ?? null)).trim().toString());
+        }).then(() => void null).finally(() => console.debug(`Attempted to remove WindowDock:\t${String(this.WindowDockName ?? "NAME_UNVAILABLE").trim()}`));
     }
 }
